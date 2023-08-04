533cfd67b4056d21b335e32ccd7773abf71a5256
baiyu <3307106692@qq.com>
2023-08-04 09:04:57 +0800

    提交rentalist

--- js/rentalList.js
+++ js/rentalList.js
@@ -23,3 +23,41 @@ for (var i = 0; i < fangxingli.length - 1; i++) {
 
     }
 }
+// 排他法写页面
+var cont = document.getElementsByClassName('cont');
+var fenlei1 = document.getElementsByClassName('fenlei1')[0];
+var fenlei1li = fenlei1.children;
+for (var i = 0; i < fenlei1li.length; i++) {
+    fenlei1li[i].setAttribute('index', i);
+    fenlei1li[i].onclick = function () {
+        var ind = this.getAttribute('index');
+        for (var j = 0; j < fenlei1li.length; j++) {
+            fenlei1li[j].classList.remove('active')
+            cont[j].classList.add('contnone');
+        }
+        fenlei1li[ind].classList.add('active')
+        cont[ind].classList.remove('contnone');
+    }
+}
+
+// 主页面  点击上面的会出现蒙版
+var fenlei = document.getElementsByClassName('fenlei')[0];
+var fenleili = fenlei.children;
+var mengban = document.getElementsByClassName('mengban')[0];
+var chaxun = document.getElementsByClassName('chaxun')[0];
+for (var i = 0; i < fenleili.length; i++) {
+    fenleili[i].setAttribute('index', i);
+    fenleili[i].onclick = function () {
+        var ind = this.getAttribute('index');
+        mengban.style.display = 'block';
+        chaxun.style.display = 'block';
+
+        fenlei1li[ind].classList.add('active')
+        cont[ind].classList.remove('contnone');
+    }
+}
+mengban.onclick = function () {
+    mengban.style.display = 'none';
+    chaxun.style.display = 'none';
+
+}
